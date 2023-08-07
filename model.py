3d029764551fd5c86183c3ad1de4017c2b18ccf3
Caetano Godinat <caetano.godinat@hotmail.com>
2023-08-07 16:57:04 +0200

    +saving fonction

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ if __name__ == "__main__":
     import numpy as np
     import pandas as pd
     from matplotlib import pyplot as plt
+    from pathlib import Path
+    import pickle
 
     data = pd.read_csv('train.csv')
 
@@ -20,12 +22,11 @@ if __name__ == "__main__":
     X_train = X_train / 255.
     _,m_train = X_train.shape
 
-    def init_params():
+    class NN(object):
         W1 = np.random.rand(10, 784) - 0.5
         b1 = np.random.rand(10, 1) - 0.5
         W2 = np.random.rand(10, 10) - 0.5
         b2 = np.random.rand(10, 1) - 0.5
-        return W1, b1, W2, b2
 
     def ReLU(Z):
         return np.maximum(Z, 0)
@@ -34,10 +35,10 @@ if __name__ == "__main__":
         A = np.exp(Z) / sum(np.exp(Z))
         return A
         
-    def forward_prop(W1, b1, W2, b2, X):
-        Z1 = W1.dot(X) + b1
+    def forward_prop(nn : NN, X):
+        Z1 = nn.W1.dot(X) + nn.b1
         A1 = ReLU(Z1)
-        Z2 = W2.dot(A1) + b2
+        Z2 = nn.W2.dot(A1) + nn.b2
         A2 = softmax(Z2)
         return Z1, A1, Z2, A2
 
@@ -50,22 +51,22 @@ if __name__ == "__main__":
         one_hot_Y = one_hot_Y.T
         return one_hot_Y
 
-    def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    def backward_prop(Z1, A1, A2, nn : NN, X, Y):
         one_hot_Y = one_hot(Y)
         dZ2 = A2 - one_hot_Y
         dW2 = 1 / m * dZ2.dot(A1.T)
         db2 = 1 / m * np.sum(dZ2)
-        dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+        dZ1 = nn.W2.T.dot(dZ2) * ReLU_deriv(Z1)
         dW1 = 1 / m * dZ1.dot(X.T)
         db1 = 1 / m * np.sum(dZ1)
         return dW1, db1, dW2, db2
 
-    def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
-        W1 = W1 - alpha * dW1
-        b1 = b1 - alpha * db1    
-        W2 = W2 - alpha * dW2  
-        b2 = b2 - alpha * db2    
-        return W1, b1, W2, b2
+    def update_params(nn : NN, dW1, db1, dW2, db2, alpha):
+        nn.W1 = nn.W1 - alpha * dW1
+        nn.b1 = nn.b1 - alpha * db1    
+        nn.W2 = nn.W2 - alpha * dW2  
+        nn.b2 = nn.b2 - alpha * db2    
+        return nn
 
     def get_predictions(A2):
         return np.argmax(A2, 0)
@@ -74,28 +75,26 @@ if __name__ == "__main__":
         print(predictions, Y)
         return np.sum(predictions == Y) / Y.size
 
-    def gradient_descent(X, Y, alpha, iterations):
-        W1, b1, W2, b2 = init_params()
+    def gradient_descent(X, Y, alpha, iterations, nn):
+        nn = NN()
         for i in range(iterations):
-            Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
-            dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-            W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+            Z1, A1, Z2, A2 = forward_prop(nn, X)
+            dW1, db1, dW2, db2 = backward_prop(Z1, A1, A2, nn, X, Y)
+            nn = update_params(nn, dW1, db1, dW2, db2, alpha)
             if i % 10 == 0:
                 print("Iteration: ", i)
                 predictions = get_predictions(A2)
                 print(get_accuracy(predictions, Y))
-        return W1, b1, W2, b2
+        return nn
 
-    W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 700)
-
-    def make_predictions(X, W1, b1, W2, b2):
-        _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    def make_predictions(X, nn : NN):
+        _, _, _, A2 = forward_prop(nn, X)
         predictions = get_predictions(A2)
         return predictions
 
-    def test_prediction(index, W1, b1, W2, b2):
+    def test_prediction(index, nn : NN):
         current_image = X_train[:, index, None]
-        prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+        prediction = make_predictions(X_train[:, index, None], nn)
         label = Y_train[index]
         print("Prediction: ", prediction)
         print("Label: ", label)
@@ -105,9 +104,22 @@ if __name__ == "__main__":
         plt.imshow(current_image, interpolation='nearest')
         plt.show()
 
-    for i in range(20):
-        test_prediction(i, W1, b1, W2, b2)
+def save(nn):
+    print("Saving model...")
 
-    dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
-    get_accuracy(dev_predictions, Y_dev)
+    with open('model.pkl', 'wb') as file:
+        pickle.dump(nn, file)
+
+def load():
+    print("Loading model...")
+    
+    with open('model.pkl', 'rb') as file:
+        loaded_model = pickle.load(file)
+    return loaded_model
+
+nn = NN()
+nn = gradient_descent(X_train, Y_train, 0.10, 700, nn)
+
+for i in range(20):
+    test_prediction(i, nn)
 
